f4b5102b7551639e57d9117ce6411579bf35df32
anushanram <hpanushan@gmail.com>
2019-05-19 15:20:35 +0530

    Creating the project files

---
+++ JavaTutorial/src/main/java/com/hpanushan/JavaTutorial/App.java
@@ -0,0 +1,13 @@
+package com.hpanushan.JavaTutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
