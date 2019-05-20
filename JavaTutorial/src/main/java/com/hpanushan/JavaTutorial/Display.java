1571c02a940720609fda338b4c8195ad4618e444
anushanram <hpanushan@gmail.com>
2019-05-20 18:04:11 +0530

    Creating the window for event handler

--- JavaTutorial/src/main/java/com/hpanushan/JavaTutorial/Display.java
+++ JavaTutorial/src/main/java/com/hpanushan/JavaTutorial/Display.java
@@ -1,21 +1,38 @@
 package com.hpanushan.JavaTutorial;
 
 import java.awt.FlowLayout;		// Layout to the window
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JFrame;		// Main window functionalities close, minimize, maximiza
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
 import javax.swing.JLabel;		// Text fields inside the window
 
 public class Display extends JFrame {
 
-	private JLabel item1,item2;
+	private JTextField item1;
+	private JTextField item2;
+	private JTextField item3;
+	private JPasswordField passwordField;
 	
+	// Constructor
 	public Display() {
 		super("Title bar");				// The titlebar
 		setLayout(new FlowLayout());	// Create a new layout
 		
-		item1 = new JLabel("Hellow world");
+		item1 = new JTextField(10);		// Text field 1
 		add(item1);
-		item2 = new JLabel("Anushan");
+		
+		item2 = new JTextField("Enter text here");	// Text field 2
 		add(item2);
+		
+		item3 = new JTextField("uneditable",20);	// Text field 3
+		item3.setEditable(false);
+		add(item3);
+		
+		passwordField = new JPasswordField("******************");	// Password field
+		add(passwordField);
 	}
 	
 }
