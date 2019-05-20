b90152f5c2f1f13c6ee878a1fbd6c2589e35cb0a
anushanram <hpanushan@gmail.com>
2019-05-20 11:57:50 +0530

    Setting military time

--- JavaTutorial/src/main/java/com/hpanushan/JavaTutorial/App.java
+++ JavaTutorial/src/main/java/com/hpanushan/JavaTutorial/App.java
@@ -4,16 +4,11 @@ public class App
 {
     public static void main( String[] args )
     {
-       System.out.println(average(1,45,7,69)); 
+    	// Object decleration
+    	TimeClass tc = new TimeClass();
+    	tc.setTime(2, 23, 56);
+    	System.out.println(tc.toMilitaryTime());
+    	
     }
     
-    public static float average(int... x) {
-    	float total = 0;
-    	
-    	for (int i:x) {
-    		total = total + i;
-    	}
-    	
-    	return total/x.length;
-    }
 }
