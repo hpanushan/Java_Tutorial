e047d900fdde7b9b538d11fc9fe237978c460e78
anushanram <hpanushan@gmail.com>
2019-05-20 11:41:43 +0530

    Arguments with varialbe length

--- JavaTutorial/src/main/java/com/hpanushan/JavaTutorial/App.java
+++ JavaTutorial/src/main/java/com/hpanushan/JavaTutorial/App.java
@@ -1,13 +1,19 @@
 package com.hpanushan.JavaTutorial;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       System.out.println(average(1,45,7,69)); 
+    }
+    
+    public static float average(int... x) {
+    	float total = 0;
+    	
+    	for (int i:x) {
+    		total = total + i;
+    	}
+    	
+    	return total/x.length;
     }
 }
